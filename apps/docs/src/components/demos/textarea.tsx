64b6c6c0868139deba82b5fe53b0f40e5e33e89f
Nikhil L <laudyanikhil@gmail.com>
2025-10-30 18:27:11 -0500

    removed email and doc folder

--- apps/docs/src/components/demos/textarea.tsx
+++
@@ -1,7 +0,0 @@
-import { Textarea } from '@repo/ui/components/textarea'
-
-const TextareaDemo = () => {
-  return <Textarea placeholder='Type your message here.' className='max-w-md' />
-}
-
-export default TextareaDemo
